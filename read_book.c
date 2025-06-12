a5aa3504754035c5d3581c0aa53e0ae998c694f5
Antonin Percot <antonin.percot@gmail.com>
2025-06-12 10:27:39 +0200

    programme qui est dans le moode pour lire le texte

--- read_book.c
+++ read_book.c
@@ -1,5 +1,24 @@
 //
 // Created by doria on 12/06/2025.
 //
-
+#include <stdio.h>
 #include "read_book.h"
+#include <stdlib.h>
+#define LINE_SIZE 512
+
+char* readBookFile(char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Unable to open file <%s>\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    char line[LINE_SIZE];
+    while(fgets(line, sizeof(line), file)) {
+
+    }
+}
+
+struct Chapter readOneChapter(char* chapterText) {
+
+}
