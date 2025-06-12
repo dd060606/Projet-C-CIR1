d3f3203383407b722821662b39d4bc8467e9fdb2
Antonin Percot <antonin.percot@gmail.com>
2025-06-12 11:02:12 +0200

    approffondissement de la fonction pour séparer les chapitres

--- read_book.c
+++ read_book.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "read_book.h"
 #include <stdlib.h>
+#include <string.h>
 #define LINE_SIZE 512
 
 char* readBookFile(char* filename) {
@@ -12,9 +13,20 @@ char* readBookFile(char* filename) {
         printf("Unable to open file <%s>\n", filename);
         exit(EXIT_FAILURE);
     }
-
+    struct Chapter chapter;
+    int saut_de_ligne=0;
     char line[LINE_SIZE];
     while(fgets(line, sizeof(line), file)) {
+        if (strcmp(line,"\n") == 0) {
+            saut_de_ligne++;
+        }
+        else {
+            saut_de_ligne=0;    //on retemet saut de ligne a 0 car sinon il comptera juste tous les sauts de ligne du texte
+        }
+        if (saut_de_ligne==2) {
+            Chapter_add()
+        }
+
 
     }
 }
