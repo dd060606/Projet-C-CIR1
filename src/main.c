3df9bec02cb9310ec49239182599a4465ed6b52d
dd060606 <dd06.developpement@gmail.com>
2025-06-18 10:52:08 +0200

    déplacement C dans src/

--- src/main.c
+++ src/main.c
@@ -5,5 +5,6 @@
 int main(void) {
     struct ChapterArray chapArray = readBookFile("../book.txt");
     writeHTML(&chapArray);
+    printf("Fichiers HTML générés dans le dossier export/chapters\n");
     return 0;
 }
